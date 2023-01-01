2dc6855eb48c7d574b097c2adab4aae353ac854e
Lakshmikant.K <l.kumar@saama.com>
2023-01-01 20:40:09 +0530

    add save message on POST req

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,48 @@
-var express = require('express');
+var express = require("express");
+const moment = require("moment");
+const { body, validationResult } = require("express-validator");
+
 var router = express.Router();
 
 const messages = [
   {
-    text: "Hi there!",
-    user: "Amando",
-    added: '16mins ago'
+    text: "Happy New year!",
+    user: "LK",
+    added: moment().startOf("year"),
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: '17mins ago'
+    added: moment().startOf("week"),
   },
 ];
 
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Mini Message Board', messages: messages });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message Board", messages: messages });
 });
 
-router.get('/new', function (req, res, next) {
-  res.render('form', { title: 'Mini Message Board', formTitle: 'Add Message' })
-})
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Mini Message Board", formTitle: "Add Message" });
+});
+
+router.post(
+  "/new",
+  body("text", "Message/text field is empty").trim().isLength({ min: 1 }),
+  body("user", "User field is empty").trim().isLength({ min: 1 }),
+  function (req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("form", {
+        title: "Mini Message Board",
+        formTitle: "Add Message",
+        errors: errors.array(),
+      });
+    } else {
+      const { text, user } = req.body;
+      messages.push({ text, user, added: moment() });
+      res.redirect("/");
+    }
+  }
+);
 
 module.exports = router;
