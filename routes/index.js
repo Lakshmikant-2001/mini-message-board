e94d70c8465cdd4e973b9472cb3f5a8127e95fb2
Lakshmikant.K <l.kumar@saama.com>
2023-01-01 13:07:18 +0530

    add message box layout and styles

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: '16mins ago'
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: '17mins ago'
+  },
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mini Message Board' });
+  res.render('index', { title: 'Mini Message Board', messages: messages });
 });
 
 module.exports = router;
